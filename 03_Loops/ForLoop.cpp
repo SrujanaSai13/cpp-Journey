5bfbc1e7235c6095107d312f017d94f724ec8c3e
Srujana Sai <srujanasai1309@gmail.com>
2026-08-07 18:52:23 +0530

    Add files via upload

---
+++ 03_Loops/ForLoop.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){ 
+    int n = 10;
+
+    for(int i=1; i<=n; i=i+2){     // for(initialization; condition; updation)
+        cout<<i<<" ";
+    }
+    cout << endl;
+    return 0;
+}
